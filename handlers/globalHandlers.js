3bf1eb6b6cc194bd187e49d03501306719cc58d7
Никита Галкин < >
2017-12-07 21:14:52 +0300

    аниме в global'e

--- handlers/globalHandlers.js
+++ handlers/globalHandlers.js
@@ -5,6 +5,9 @@ const globalHandlers = {
     "хуй|пизда|мудак|еба|ёба|пидор|бля": async function (text, author, match) {
         if (!author.extra.admin)
             return "**НЕ МАТЕРИСЬ**"
+    },
+    "аниме|мультик|антон": async function (text, author, match) {
+        if (Math.random()>0.5) {return 'сам ты аниме'} else {return 'НЕНАВИЖУ АНИМЕ, ОНО ДЛЯ ДАУНОВ'}
     }
 }
 module.exports = globalHandlers
